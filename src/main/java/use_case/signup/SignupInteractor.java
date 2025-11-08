0f986cf4e1b4e5f7ce0db5e168192a2102c08a3d
jameshuang3013 <jameshuang3013@hotmail.com>
2025-11-08 01:36:23 -0500

    Implement UserFactory and SignupInteractor

--- src/main/java/use_case/signup/SignupInteractor.java
+++ src/main/java/use_case/signup/SignupInteractor.java
@@ -1,4 +1,53 @@
 package use_case.signup;
 
-public class SignupInteractor {
+import entity.User;
+import entity.UserFactory;
+
+public class SignupInteractor implements SignupInputBoundary {
+
+    private final SignupUserDataAccessInterface userDataAccessObject;
+    private final SignupOutputBoundary userPresenter;
+    private final UserFactory userFactory;
+
+    public SignupInteractor(SignupUserDataAccessInterface userDataAccessObject,
+                            SignupOutputBoundary userPresenter,
+                            UserFactory userFactory) {
+        this.userDataAccessObject = userDataAccessObject;
+        this.userPresenter = userPresenter;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(SignupInputData inputData) {
+        String username = inputData.getUsername();
+        String password = inputData.getPassword();
+        String repeatPassword = inputData.getRepeatPassword();
+        String preferredLanguage = inputData.getPreferredLanguage();
+
+        // Validation using else-if structure
+        if (username.isEmpty()) {
+            userPresenter.prepareFailView("Username cannot be empty");
+        } else if (password.isEmpty()) {
+            userPresenter.prepareFailView("Password cannot be empty");
+        } else if (!password.equals(repeatPassword)) {
+            userPresenter.prepareFailView("Passwords don't match.");
+        } else if (userDataAccessObject.existsByName(username)) {
+            userPresenter.prepareFailView("User already exists.");
+        } else {
+            // Create the User
+            User user = userFactory.create(username, password, preferredLanguage);
+
+            // Persist the User
+            userDataAccessObject.save(user);
+
+            // Prepare output
+            SignupOutputData outputData = new SignupOutputData(user.getUsername());
+            userPresenter.prepareSuccessView(outputData);
+        }
+    }
+
+    @Override
+    public void switchToLoginView() {
+        userPresenter.switchToLoginView();
+    }
 }

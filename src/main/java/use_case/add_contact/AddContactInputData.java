5bad8c61c5c0b69eb630dcf5cbdee20c6a2fe59d
saks <angelachen1260@gmail.com>
2025-11-10 17:34:30 -0500

    implemented AddContactInputData and AddContactOutput Data

--- src/main/java/use_case/add_contact/AddContactInputData.java
+++ src/main/java/use_case/add_contact/AddContactInputData.java
@@ -1,4 +1,14 @@
 package use_case.add_contact;
 
 public class AddContactInputData {
+
+    private final int userId;
+
+    public AddContactInputData(int userId) {
+        this.userId = userId;
+    }
+
+    int getUserId() {
+        return userId;
+    }
 }
--- src/main/java/use_case/add_contact/AddContactOutputData.java
+++ src/main/java/use_case/add_contact/AddContactOutputData.java
@@ -1,4 +1,13 @@
 package use_case.add_contact;
 
 public class AddContactOutputData {
+    private int userID;
+
+    public AddContactOutputData(int userID) {
+        this.userID = userID;
+    }
+
+    int getUserID() {
+        return userID;
+    }
 }

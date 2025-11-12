4df7a643f88719d966c9fa6ebf5abf15959a22f9
jameshuang3013 <jameshuang3013@hotmail.com>
2025-11-12 01:48:03 -0500

    Implement ViewModel and ViewManagerModel

--- src/main/java/interface_adapter/ViewModel.java
+++ src/main/java/interface_adapter/ViewModel.java
@@ -1,5 +1,39 @@
 package interface_adapter;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class ViewModel<A> {
 
+    private final String viewName;
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+    private A state;
+
+    public ViewModel(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public String getViewName() {
+        return this.viewName;
+    }
+
+    public A getState() {
+        return this.state;
+    }
+
+    public void setState(A state) {
+        this.state = state;
+    }
+
+    public void firePropertyChange() {
+        this.support.firePropertyChange("state", null, this.state);
+    }
+
+    public void firePropertyChange(String propertyName) {
+        this.support.firePropertyChange(propertyName, null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        this.support.addPropertyChangeListener(listener);
+    }
 }

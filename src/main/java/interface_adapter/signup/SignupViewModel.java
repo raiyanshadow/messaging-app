701206c4a8b6133ba1f5e5e4c74693384b262491
jameshuang3013 <jameshuang3013@hotmail.com>
2025-11-13 00:30:38 -0500

    Implement SignupViewModel, SignupState, SignupView

--- src/main/java/interface_adapter/signup/SignupViewModel.java
+++ src/main/java/interface_adapter/signup/SignupViewModel.java
@@ -1,4 +1,26 @@
 package interface_adapter.signup;
 
-public class SignupViewModel {
+import interface_adapter.ViewModel;
+
+public class SignupViewModel extends ViewModel<SignupState> {
+
+    public static final String TITLE_LABEL = "Sign Up View";
+    public static final String USERNAME_LABEL = "Choose username";
+    public static final String PASSWORD_LABEL = "Choose password";
+    public static final String REPEAT_PASSWORD_LABEL = "Enter password again";
+    public static final String LANGUAGE_LABEL = "Select language";
+
+    public static final String SIGNUP_BUTTON_LABEL = "Sign up";
+    public static final String TO_LOGIN_BUTTON_LABEL = "Go to Login";
+
+    // Default languages for dropdown
+    public static final String[] LANGUAGES = {"English", "French", "Spanish"};
+
+    public SignupViewModel() {
+        super("sign up");
+        SignupState initialState = new SignupState();
+        // Set default language
+        initialState.setPreferredLanguage(LANGUAGES[0]);
+        setState(initialState);
+    }
 }

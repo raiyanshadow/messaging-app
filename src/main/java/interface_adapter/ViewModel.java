06b1f465e6bf6d9bf9396776aa3392a1d0822ad5
raiyanshadow <taseenraiyan@gmail.com>
2025-11-14 01:11:40 -0500

    fix files and adapt naming conventions

--- src/main/java/interface_adapter/ViewModel.java
+++ src/main/java/interface_adapter/ViewModel.java
@@ -5,34 +5,6 @@ import java.beans.PropertyChangeSupport;
 
 public class ViewModel<A> {
     private final String viewName;
-    protected final PropertyChangeSupport support = new PropertyChangeSupport(this);
-    private A state;
-
-    public ViewModel(String viewName) {
-        this.viewName = viewName;
-    }
-
-    public String getViewName() {
-        return this.viewName;
-    }
-
-    public A getState() {
-        return this.state;
-    }
-
-    public void setState(A state) {
-        this.state = state;
-    }
-
-    public void firePropertyChange() {
-        this.support.firePropertyChange("state", null, this.state);
-    }
-
-    public void firePropertyChange(String propertyName) {
-        this.support.firePropertyChange(propertyName, null, this.state);
-    }
-
-    private final String viewName;
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
     private A state;
 
--- src/main/java/use_case/send_message/SendMessageInputBoundary.java
+++ src/main/java/use_case/send_message/SendMessageInputBoundary.java
@@ -1,5 +1,5 @@
 package use_case.send_message;
 
 public interface SendMessageInputBoundary {
-    void sendTextMessage(SendMessageInputData sendMessageInputData);
+    void execute(SendMessageInputData sendMessageInputData);
 }

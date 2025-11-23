8aacdb5c79600eb62eec0be730e56eecc860a80d
Salem Baazeem <salem_mb@icloud.com>
2025-11-22 20:25:54 -0500

    pushing what I was meant to push a week ago but due to high conflict traffic I couldn't :(

--- src/main/java/use_case/baseUI/BaseUIOutputBoundary.java
+++ src/main/java/use_case/baseUI/BaseUIOutputBoundary.java
@@ -5,5 +5,6 @@ import entity.DirectChatChannel;
 import java.util.List;
 
 public interface BaseUIOutputBoundary {
-   public void DisplayUI(BaseUIOutputData response);
+    void DisplayUI(BaseUIOutputData response);
+    void DisplayAddChat(BaseUIOutputData response);
 }

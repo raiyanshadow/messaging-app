2df9a7f2a0d3833aac9316076c485dcfbbf101a0
Sophia Xie <ningyi.xie@mail.utoronto.ca>
2025-11-29 04:29:07 -0500

    Add SearchContactUserDataAccessInterface

--- src/main/java/use_case/search_contact/SearchContactUserDataAccessInterface.java
+++ src/main/java/use_case/search_contact/SearchContactUserDataAccessInterface.java
@@ -1,4 +1,10 @@
 package use_case.search_contact;
 
+import entity.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface SearchContactUserDataAccessInterface {
+    List<User> searchUsers(String keyword) throws SQLException;
 }

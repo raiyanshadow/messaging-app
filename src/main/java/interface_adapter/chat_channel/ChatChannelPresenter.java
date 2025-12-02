5237c4c931bb90d3d0fda1b5bd819b4e2d4523bc
jameshuang3013 <jameshuang3013@hotmail.com>
2025-12-02 14:04:37 -0500

    fixes

--- src/main/java/interface_adapter/chat_channel/ChatChannelPresenter.java
+++ src/main/java/interface_adapter/chat_channel/ChatChannelPresenter.java
@@ -1,16 +1,9 @@
 package interface_adapter.chat_channel;
 
 import use_case.send_message.SendMessageOutputBoundary;
-import use_case.edit_message.EditMessageOutputBoundary;
-import use_case.reply_message.ReplyMessageOutputBoundary;
-import use_case.delete_message.DeleteMessageOutputBoundary;
 import use_case.send_message.SendMessageOutputData;
-import use_case.edit_message.EditMessageOutputData;
-import use_case.reply_message.ReplyMessageOutputData;
-import use_case.delete_message.DeleteMessageOutputData;
 
-public class ChatChannelPresenter implements SendMessageOutputBoundary, EditMessageOutputBoundary,
-                                             ReplyMessageOutputBoundary, DeleteMessageOutputBoundary {
+public class ChatChannelPresenter implements SendMessageOutputBoundary{
 
     private final MessageViewModel messageViewModel;
 
@@ -36,64 +29,4 @@ public class ChatChannelPresenter implements SendMessageOutputBoundary, EditMess
         messageViewModel.firePropertyChange();
     }
 
-    @Override
-    public void prepareEditMessageSuccessView(EditMessageOutputData outputData) {
-        MessageState messageState = messageViewModel.getState();
-        messageState.setContent(outputData.getNewContent());
-        messageState.setChannelURL(outputData.getChannelUrl());
-        messageState.setSenderID(outputData.getSenderId());
-        messageState.setReceiverID(outputData.getReceiverId());
-        messageState.setTimestamp(outputData.getOldTimestamp());
-        messageState.setEditedTimestamp(outputData.getNewTimestamp());
-        messageState.setEdited(true);
-        messageViewModel.firePropertyChange();
-    }
-
-    @Override
-    public void prepareEditMessageFailView(String error) {
-        MessageState messageState = messageViewModel.getState();
-        messageState.setError(error);
-        messageViewModel.firePropertyChange();
-    }
-
-    @Override
-    public void prepareReplyMessageSuccessView(ReplyMessageOutputData outputData) {
-        MessageState messageState = messageViewModel.getState();
-        messageState.setContent(outputData.getContent());
-        messageState.setChannelURL(outputData.getChannelUrl());
-        messageState.setSenderID(outputData.getSenderId());
-        messageState.setReceiverID(outputData.getReceiverId());
-        messageState.setTimestamp(outputData.getTimestamp());
-        messageState.setReplied(true);
-        messageViewModel.firePropertyChange();
-    }
-
-    @Override
-    public void prepareReplyMessageFailView(String error) {
-        MessageState messageState = messageViewModel.getState();
-        messageState.setError(error);
-        messageViewModel.firePropertyChange();
-    }
-
-    @Override
-    public void prepareDeleteMessageSuccessView(DeleteMessageOutputData outputData) {
-        MessageState messageState = messageViewModel.getState();
-        messageState.setContent(null);
-        messageState.setChannelURL(null);
-        messageState.setReceiverID(null);
-        messageState.setSenderID(null);
-        messageViewModel.firePropertyChange();
-    }
-
-    @Override
-    public void prepareDeleteMessageFailView(String error) {
-        MessageState messageState = messageViewModel.getState();
-        messageState.setError(error);
-        messageViewModel.firePropertyChange();
-    }
-
-    public void prepareReturnToHome() {
-        MessageState messageState = messageViewModel.getState();
-        messageViewModel.firePropertyChange();
-    }
 }
